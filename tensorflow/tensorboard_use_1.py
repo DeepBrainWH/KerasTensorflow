b8bfd19ca1f12bec6c3a6839bbcb93d598d0df3e
wangheng <673602165@qq.com>
2019-03-18 00:32:47 +0800

    add conv3

--- tensorflow/tensorboard_use_1.py
+++ tensorflow/tensorboard_use_1.py
@@ -21,44 +21,62 @@ with tf.name_scope("input"):
 # ========================conv 1============================
 with tf.name_scope("conv_1"):
     with tf.device(device1):
-        kernel = tf.random.normal([5, 5, 3, 32], name="kernel_1")
-        bias = tf.random.normal([32])
-        conv_1_1 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(X_, kernel, [1, 2, 2, 1], 'SAME'), bias), name='conv_1_1')
+        kernel1_1 = tf.random.normal([5, 5, 3, 32], name="kernel_1")
+        bias1_1 = tf.random.normal([32])
+        conv_1_1 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(X_, kernel1_1, [1, 2, 2, 1], 'SAME'), bias1_1), name='conv_1_1')
 
         pool_1_1 = tf.nn.max_pool(X_, [1, 3, 3, 1], [1, 2, 2, 1], 'SAME', name="max_pool_1_1")
 
-        kernel2 = tf.random.normal([3, 3, 3, 16], name='kernel_2')
-        bias2 = tf.random.normal([16], name='bias2')
-        conv_1_2 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(X_, kernel2, [1, 1, 1, 1], 'SAME'), bias2), name="conv_1_2")
+        kernel1_2 = tf.random.normal([3, 3, 3, 16], name='kernel_2')
+        bias1_2 = tf.random.normal([16], name='bias2')
+        conv_1_2 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(X_, kernel1_2, [1, 1, 1, 1], 'SAME'), bias1_2), name="conv_1_2")
         pool_1_2 = tf.nn.max_pool(conv_1_2, [1, 3, 3, 1], [1, 2, 2, 1], 'SAME', name='max_pool_1_2')
 
-        concat_value = tf.concat([conv_1_1, pool_1_1, pool_1_2], 3, 'concat_tensor')
+        concat_value1 = tf.concat([conv_1_1, pool_1_1, pool_1_2], 3, 'concat_tensor')
         # size: (None, 153, 204, 51)
-        conv_1_shape = concat_value.shape.as_list()
+        conv_1_shape = concat_value1.shape.as_list()
+        print("============conv1 shape:   ", conv_1_shape, "==================")
 
 # ========================conv 2==============================
 with tf.name_scope("conv_2"):
-    with tf.device(device1):
-        kernel = tf.random.normal([5, 5, conv_1_shape[3], 64], name="kernel_1")
-        bias = tf.random.normal([64])
-        conv_2_1 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(X_, kernel, [1, 2, 2, 1], 'SAME'), bias), name='conv_1_1')
-
-        pool_2_1 = tf.nn.max_pool(X_, [1, 3, 3, 1], [1, 2, 2, 1], 'SAME', name="max_pool_1_1")
-
-        kernel2 = tf.random.normal([3, 3, conv_1_shape[3], 16], name='kernel_2')
-        bias2 = tf.random.normal([16], name='bias2')
-        conv_2_2 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(X_, kernel2, [1, 1, 1, 1], 'SAME'), bias2), name="conv_1_2")
-        pool_2_2 = tf.nn.max_pool(conv_2_2, [1, 3, 3, 1], [1, 2, 2, 1], 'SAME', name='max_pool_1_2')
-
-        concat_value = tf.concat([conv_2_1, pool_2_1, pool_2_2], 3, 'concat_tensor')
-        conv_2_shape = concat_value.shape.as_list()
-
-with tf.name_scope("conv3"):
     with tf.device(device2):
-        kernel = tf.random.norma()
+        kernel2_1 = tf.random.normal([5, 5, conv_1_shape[3], 64], name="kernel_1")
+        bias2_1 = tf.random.normal([64])
+        conv_2_1 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(concat_value1, kernel2_1, [1, 2, 2, 1], 'SAME'), bias2_1), name='conv_1_1')
+
+        pool_2_1 = tf.nn.max_pool(concat_value1, [1, 3, 3, 1], [1, 2, 2, 1], 'SAME', name="max_pool_2_1")
+
+        kernel2_2 = tf.random.normal([3, 3, conv_1_shape[3], 16], name='kernel_2')
+        bias2_2 = tf.random.normal([16], name='bias2')
+        conv_2_2 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(concat_value1, kernel2_2, [1, 1, 1, 1], 'SAME'), bias2_2), name="conv_1_2")
+        pool_2_2 = tf.nn.max_pool(conv_2_2, [1, 3, 3, 1], [1, 2, 2, 1], 'SAME', name='max_pool_2_2')
+
+        concat_value2 = tf.concat([conv_2_1, pool_2_1, pool_2_2], 3, 'concat_tensor')
+        conv_2_shape = concat_value2.shape.as_list()
+        print("============conv2 shape:   ", conv_2_shape, "==================")
+
+# =======================CONV 3================================
+with tf.name_scope("conv3"):
+    with tf.device(device3):
+        kernel3_1 = tf.random.normal([5,5, conv_2_shape[3], 32], name='kernel')
+        bias3_1 = tf.zeros([32], name='bias3_1')
+        conv3_1 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(concat_value2, kernel3_1, [1, 2, 2, 1], 'SAME')), name='conv3_1')
+
+        pool3_1 = tf.nn.max_pool(concat_value2, [1, 3, 3, 1], [1, 2, 2, 1], 'SAME', name='max_pool_3_1')
+
+        kernel3_2 = tf.random.normal([3,3, conv_2_shape[3], 16], name='kernel3_2')
+        bias3_2 = tf.random.normal([16], name='bias3_2')
+        conv3_2 = tf.nn.relu(tf.nn.bias_add(tf.nn.conv2d(concat_value2, kernel3_2, [1, 1, 1, 1], 'SAME'), bias3_2), name="conv_3_2")
+        pool3_2 = tf.nn.max_pool(conv3_2, [1, 3, 3, 1], [1,2,2,1], 'SAME', name='max_pool3_2')
+
+        concat_value3 = tf.concat([conv3_1, pool3_1, pool3_2], 3, name='concat3')
+
+        conv3_shape = concat_value3.shape.as_list()
+        print("============conv3 shape:   ", conv3_shape, "==================")
+
 
 with tf.name_scope("full_connection_1"):
-    with tf.device("/GPU:0"):
+    with tf.device(device0):
         w1 = tf.Variable(tf.random.normal([306, 128]), name="w1")
         b1 = tf.Variable(tf.random.normal([128]), name="b1")
         result = tf.nn.bias_add(tf.matmul(X_, w1, name="mat_mul"), b1, name="biase_add")
